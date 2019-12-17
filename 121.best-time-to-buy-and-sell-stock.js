caf7578eb8f96ac873134dbcd31e0c3264076741
Shiyu Wang <shiyu3169@gmail.com>
2019-12-17 15:43:25 -0500

    121, 122

---
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=121 lang=javascript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = 0;
+  for (let i = 0; i < prices.length; i++) {
+    min = Math.min(min, prices[i]);
+    max = Math.max(max, prices[i] - min);
+  }
+  return max;
+};
+// @lc code=end
+// 200/200 cases passed (56 ms)
+// Your runtime beats 87.46 % of javascript submissions
+// Your memory usage beats 44.44 % of javascript submissions (35.5 MB)
---
+++ 122.best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=122 lang=javascript
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let diff = 0;
+  if (prices.length > 0) {
+    prices.reduce((acc, next) => {
+      if (next > acc) {
+        diff += next - acc;
+      }
+      return next;
+    });
+  }
+  return diff;
+};
+// @lc code=end
+// 201/201 cases passed (64 ms)
+// Your runtime beats 38.49 % of javascript submissions
+// Your memory usage beats 90.48 % of javascript submissions (35.3 MB)
