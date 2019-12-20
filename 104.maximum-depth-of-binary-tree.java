a55c3bcecca260189d3494d984f9575015297de5
Shiyu Wang <shiyu3169@gmail.com>
2019-12-20 15:34:02 -0500

    104

---
+++ 104.maximum-depth-of-binary-tree.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=104 lang=java
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+// @lc code=end
+// 39/39 cases passed (0 ms)
+// Your runtime beats 100 % of java submissions
+// Your memory usage beats 96.24 % of java submissions (38.5 MB)
---
+++ 104.maximum-depth-of-binary-tree.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=104 lang=javascript
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (!root) {
+    return 0;
+  }
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+// @lc code=end
+// 39/39 cases passed (60 ms)
+// Your runtime beats 79.6 % of javascript submissions
+// Your memory usage beats 37.5 % of javascript submissions (37.2 MB)
